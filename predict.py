ffd253caad3534778480bc06074f9c3de10bb7d0
tianjinhai141 <73387113+tianjinhai141@users.noreply.github.com>
2021-04-18 14:25:15 +0800

    Add files via upload

---
+++ predict.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+from PIL import Image
+
+from frcnn import FRCNN
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+gpus = tf.config.experimental.list_physical_devices(device_type='GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+
+frcnn = FRCNN()
+
+while True:
+    img = input('Input image filename:')
+    try:
+        image = Image.open(img)
+    except:
+        print('Open Error! Try again!')
+        continue
+    else:
+        r_image = frcnn.detect_image(image)
+        r_image.show()
+        
+    
